94cdbc194cd53f00105234e9dfc9825e4c9cd549
Zac Mueterthies <zjm@iastate.edu>
2024-12-02 16:03:57 -0600

    simple script to remove WMP metadata files

---
+++ wmp-cleaner.sh
@@ -0,0 +1,12 @@
+#! /bin/zsh
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+echo -e "${bold}Removing desktop.ini files${normal}"
+
+find . -type f -name desktop.ini -exec rm -i {} \; 
+
+echo -e "${bold}Removing WMP album art files${normal}"
+
+find . -type f \( -name "AlbumArt*.jpg" -o -name "Folder.jpg" \) -exec echo Deleting: {} \;  -exec rm -i {} \; 
