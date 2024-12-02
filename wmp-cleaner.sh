201343b101e54d6aea68f17b5325e94f5436331a
Zac Mueterthies <zjm@iastate.edu>
2024-12-02 16:34:45 -0600

    combined commands

--- wmp-cleaner.sh
+++ wmp-cleaner.sh
@@ -3,10 +3,6 @@
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-echo -e "${bold}Removing desktop.ini files${normal}"
+echo -e "${bold}Removing Windows & WMP album art files${normal}"
 
-find . -type f -name desktop.ini -exec rm {} \; 
-
-echo -e "${bold}Removing WMP album art files${normal}"
-
-find . -type f \( -name "AlbumArt*.jpg" -o -name "Folder.jpg" \) -exec echo Deleting: {} \;  -exec rm {} \; 
+find . -type f \( -name desktop.ini -o -name thumbs.db -o -name "AlbumArt*.jpg" -o -name Folder.jpg \) -exec echo Deleting: {} \;  -exec rm {} \; 
