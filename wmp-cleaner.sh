9a679fabc622ee54794366938e9768e48f75e32f
Zac Mueterthies <zjm@iastate.edu>
2024-12-02 16:28:55 -0600

    removed prompt for deletion

--- wmp-cleaner.sh
+++ wmp-cleaner.sh
@@ -5,8 +5,8 @@ normal=$(tput sgr0)
 
 echo -e "${bold}Removing desktop.ini files${normal}"
 
-find . -type f -name desktop.ini -exec rm -i {} \; 
+find . -type f -name desktop.ini -exec rm {} \; 
 
 echo -e "${bold}Removing WMP album art files${normal}"
 
-find . -type f \( -name "AlbumArt*.jpg" -o -name "Folder.jpg" \) -exec echo Deleting: {} \;  -exec rm -i {} \; 
+find . -type f \( -name "AlbumArt*.jpg" -o -name "Folder.jpg" \) -exec echo Deleting: {} \;  -exec rm {} \; 
